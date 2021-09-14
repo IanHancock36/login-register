08c6c1533ed8de246e6bd25df3b29902dc117df7
Ian Hancock <northroadian@gmail.com>
2021-09-14 06:52:03 -0500

    styles

--- components/styles.js
+++ components/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import {View} from 'react-native'
+import {View,Text,Image} from 'react-native'
 import Constants from 'expo-constants'
 // colors 
 const StatusBarHeight = Constants.statusBarHeight 
@@ -20,4 +20,23 @@ flex: 1;
 padding: 25px ;
 background-color: ${primary}
 padding-top:${StatusBarHeight + 10}px
+`
+export const InnerContainer = styled.View `
+flex: 1;
+width: 100%;
+align-items: center;
+`
+
+export const PageLogo = styled.Image`
+width:250px;
+height: 200px;
+`
+
+export const PageTitle = styled.Text `
+font-size: 30px;
+text-align: center;
+font-weight: bold;
+color: ${brand};
+padding: 10px;
+
 `
---
+++ screens/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    StyledContainer,InnerContainer,PageLogo,PageTitle
+} from './../components/styles'
+const Login = () => {
+    return(
+        <StyledContainer>
+        <InnerContainer>
+            <PageLogo />
+        </InnerContainer>
+        
+        
+        </StyledContainer>
+
+    )
+}
+
+export default Login 
