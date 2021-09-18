8ebeed6426555c85732147cd21d827b4e1347498
Ian Hancock <northroadian@gmail.com>
2021-09-18 07:04:33 -0500

    all the styling in the world

--- screens/Login.js
+++ screens/Login.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import {
-    StyledContainer,InnerContainer,PageLogo,PageTitle,SubTitle,StyledFormArea
+    StyledContainer, InnerContainer, PageLogo, PageTitle, SubTitle, StyledFormArea
 } from './../components/styles'
+import {View} from 'react-native';
 import { StatusBar } from 'expo-status-bar'
 import { Formik } from 'formik'
 const Login = () => {
-    return(
+    return (
         <StyledContainer >
-            <StatusBar style ='dark'/>
-        <InnerContainer>
-            <PageLogo resizeMode='cover'/>
-            <PageTitle>M</PageTitle>
-            <SubTitle>Account Login</SubTitle>
-            <Formik></Formik>
-        </InnerContainer>
-        
-        
+            <StatusBar style='dark' />
+            <InnerContainer>
+                <PageLogo resizeMode='cover' source ={require("./../assets/19012019-02.jpg")}/>
+                <PageTitle>M</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+                <Formik></Formik>
+            </InnerContainer>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+            >
+                {({ handleChange, handleBlur, handleSubmit, values }) => <StyledFormArea></StyledFormArea>}
+            </Formik>
         </StyledContainer>
-
+    )
+}
+const TextInput = ({label,icon,...props}) => {
+    return (
+        <View>
+            
+        </View>
     )
 }
 
-export default Login 
+export default Login
 
-// source ={require("https://picsum.photos/200")}
