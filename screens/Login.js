3c2e5d74a3294b7f2816a57b5d10d3a599d797c8
Ian Hancock <northroadian@gmail.com>
2021-09-19 07:06:17 -0500

    ui

--- screens/Login.js
+++ screens/Login.js
@@ -1,39 +1,61 @@
 import React from 'react'
 import {
-    Colors,StyledContainer,InnerContainer,WelcomeContainer, PageLogo, Avatar,WelcomeImage, PageTitle,SubTitle,StyledTextInput,StyledInputLabel, LeftIcon,RightIcon,StyledButton, ButtonText,MsgBox,Line,StyledFormArea,ExtraView,ExtraText, TextLink,TextLinkContent
+    Colors, StyledContainer, InnerContainer, WelcomeContainer, PageLogo, Avatar, WelcomeImage, PageTitle, SubTitle, StyledTextInput, StyledInputLabel, LeftIcon, RightIcon, StyledButton, ButtonText, MsgBox, Line, StyledFormArea, ExtraView, ExtraText, TextLink, TextLinkContent
 } from './../components/styles'
 import { View } from 'react-native';
-import{ Octicons } from '@expo/vector-icons'
+import { Octicons } from '@expo/vector-icons'
 import { StatusBar } from 'expo-status-bar'
 import { Formik } from 'formik'
+const { brand, darkLight } = Colors
 const Login = () => {
     return (
         <StyledContainer >
             <StatusBar style='dark' />
             <InnerContainer>
-                <PageLogo resizeMode='cover' source ={require("./../assets/19012019-02.jpg")}/>
+                <PageLogo resizeMode='cover' source={require("./../assets/19012019-02.jpg")} />
                 <PageTitle>M</PageTitle>
                 <SubTitle>Account Login</SubTitle>
                 <Formik></Formik>
             </InnerContainer>
             <Formik
                 initialValues={{ email: '', password: '' }}
-                onSubmit={(values) => {
-                    console.log(values)
-                }}
+                onSubmit={(values) => {console.log(values)}}
             >
-                {({ handleChange, handleBlur, handleSubmit, values }) => <StyledFormArea></StyledFormArea>}
+                {({ handleChange, handleBlur, handleSubmit, values }) => (<StyledFormArea>
+                    <MyTextInput
+                        label='Email Address'
+                        icon='mail'
+                        placeholder='Email Address'
+                        placeholderTextColr={darkLight}
+                        onChangText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        value={values.email}
+                        keyboardType='email-address'
+                    />
+                    <MyTextInput
+                        label='Password'
+                        icon='lock'
+                        placeholder='*********'
+                        placeholderTextColr={darkLight}
+                        onChangText={handleChange('password')}
+                        onBlur={handleBlur('')}
+                        value={values.password}
+                        secureTextEntry ={true}
+                    />
+                </StyledFormArea>)}
             </Formik>
         </StyledContainer>
     )
 }
-const TextInput = ({label,icon,...props}) => {
+const TextInput = ({ label, icon, ...props }) => {
     return (
         <View>
             <LeftIcon>
-<Octicons name={icon} size ={30}/>
+                <Octicons name={icon} size={30} color={brand} />
             </LeftIcon>
-            
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+
         </View>
     )
 }
